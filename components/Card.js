9cb76bca7415b6c23d516c96cf2c5fd2f23eb322
John Canero <johncanero.official@gmail.com>
2022-09-21 00:42:42 +0800

    Card and CardProps for Props Feature

---
+++ components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardProps from './CardProps'
+
+const Card = () => {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default Card
---
+++ components/CardProps.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardProps = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default CardProps
