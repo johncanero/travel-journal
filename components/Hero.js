9e130ef1b8ae306a9426919415c1f6dad81966cf
John Canero <johncanero.official@gmail.com>
2022-09-22 11:31:30 +0800

    Modify Hero Image

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
-import BoholLake from '../public/images/boholLake.jpg'
+import BoholHongkong from '../public/images/boholHongkong.jpg'
 
 
 const Hero = () => {
   return (
     <div>
-        <div className='mx-auto w-10/12 mt-6 text-zero md:w-9/12 md:mt-10 lg:w-4/12'>
+        <div className='mx-auto w-80 mt-6 text-zero md:w-9/12 md:mt-10 lg:w-4/12'>
             <Image
-                src={BoholLake}
+                src={BoholHongkong}
                 alt="travelHero.jpg"
                 className='rounded-3xl'
             />
