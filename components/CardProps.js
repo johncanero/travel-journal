92723e4cfd12f4519b409d739061c89906cc78c6
John Canero <johncanero.official@gmail.com>
2022-09-22 11:26:19 +0800

    Mobile Responsive Layout

--- components/CardProps.js
+++ components/CardProps.js
@@ -3,39 +3,40 @@ import Image from "next/image";
 import Link from "next/link";
 
 const CardProps = (props) => {
-  return (
+  return (  
     <div>
-      <div className="flex justify-center font-Poppins md:mt-5 md:w-auto">
+      <div className="flex justify-center font-Poppins mt-4 md:mt-2 md:w-auto">
         <div className="flex flex-col items-center bg-white rounded-lg md:flex-row md:max-w-xl">
-          <div className="object-cover w-full h-auto text-zero md:h-auto md:w-full">
+          <div className="flex justify-center object-cover w-11/12 h-auto text-zero md:h-auto md:w-full">
             <Image
-              width={300}
-              height={370}
+              width={310}
+              height={380}
               src={props.img}
               alt=""
               className="rounded-2xl"
             />
           </div>
 
-          <div className="flex flex-col justify-between p-4 leading-normal">
+          <div className="flex flex-col justify-between mt-3 px-10 md:p-4 leading-normal">
             <div className="flex">
               <p className="text-brightViolet">
                 <span>•</span>
-                {props.country}
+                {props.country} 
               </p>
 
-               <a target="_blank" rel="noopener noreferrer">
-                  <p className=" text-gray-400 md:px-3 underline hover:underline">
-                    View on Google Maps
-                  </p>
-                </a>
-           
+                <Link href='https://www.google.com/maps' passHref>
+                    <a target="_blank" rel="noopener noreferrer">
+                      <p className=" text-gray-400 px-3 md:px-3 underline hover:underline">
+                        View on Google Maps
+                      </p>
+                    </a>
+                </Link>
             </div>
 
-            <h5 className="text-4xl font-bold text-gray-900 md:mt-2">
+            <h5 className="text-4xl font-bold text-gray-900 mt-2">
               {props.place}
             </h5>
-            <div className="md:mt-5">
+            <div className="mt-4">
               <p className="font-bold">{props.date}</p>
               <p className="font-normal text-gray-700 md:mt-2">
                 {props.description}
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faEarthAsia} from '@fortawesome/free-solid-svg-icons'
@@ -11,7 +10,7 @@ const Navbar = () => {
         <nav className="bg-cyanColor border-gray-200 px-2 sm:px-4 py-5 md:py-6">
             <div className='flex justify-center'>
                 <Link href='' passHref>
-                    <a target="_blank" rel="noopener noreferrer">
+                    <a target="_blank" rel="noopener neoreferrer">
                         <div>   
                              <h1 className='flex text-brightViolet font-Poppins font-semibold'><FontAwesomeIcon icon={faEarthAsia} className='w-7 mx-2'></FontAwesomeIcon> my travel journal.</h1>
                         </div>
