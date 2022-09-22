dd7affccada58fe0de4e9a75ab5abd5e9c4af823
John Canero <johncanero.official@gmail.com>
2022-09-22 23:52:51 +0800

    Add Card Images

--- components/Card.js
+++ components/Card.js
@@ -2,6 +2,10 @@ import React from 'react'
 import CardProps from './CardProps'
 import HongkongPortrait from "../public/images/hongkongPortrait.jpg";
 import BoracayPortrait from "../public/images/boracayPortrait.jpg";
+import BoholPortrait from '../public/images/boholPortrait.jpg'
+import SingaporePortrait from '../public/images/singaporePortrait.jpg'
+import CebuPortrait from '../public/images/cebuMactan.jpg'
+import ThailandTemple from '../public/images/thailandTemple.jpg'
 
 const Card = () => {
   return (
@@ -18,13 +22,54 @@ const Card = () => {
         
         <div>
             <CardProps 
-                img={HongkongPortrait}
-                country=" HONGKONG"
-                place="HongKong Skyline"
-                date="26 Jan, 2016 -  30 Jan, 2016"
+                img={SingaporePortrait}
+                country=" SINGAPORE"
+                place="Marina Bay Sands"
+                date="26 Jan, 2015 -  30 Jan, 2015"
                 description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
             />
         </div>    
+
+              
+        <div>
+            <CardProps 
+                img={BoholPortrait}
+                country=" PHILIPPINES"
+                place="Bohol"
+                date="26 July, 2019 -  30 July, 2019"
+                description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+            />
+        </div>   
+
+        <div>
+            <CardProps 
+                img={HongkongPortrait}
+                country=" HONGKONG"
+                place="HongKong Skyline"
+                date="26 July, 2019 -  30 July, 2019"
+                description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+            />
+        </div>  
+
+        <div>
+            <CardProps 
+                img={CebuPortrait}
+                country=" PHILIPPINES"
+                place="Cebu Mactan"
+                date="26 July, 2019 -  30 July, 2019"
+                description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+            />
+        </div>    
+
+        <div>
+            <CardProps 
+                img={ThailandTemple}
+                country=" THAILAND"
+                place="Thailand Temple"
+                date="1 Jan, 2019 -  4 Jan, 2019"
+                description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+            />
+        </div>  
     </div>
   )
 }
--- components/Input.js
+++ components/Input.js
@@ -7,17 +7,17 @@ const Input = () => {
     <div>
         <div>
             <form>
-                <div class="relative mt-4">
+                <div class="relative block mt-4">
                     <input
                         type="search"
                         id="search"
-                        className="flex mx-auto p-4 pr-36 text-sm md:pr-96 md:text-base text-gray-900 bg-gray-50 rounded-3xl border border-gray-400 focus:ring-brightViolet focus:border-brightViolet dark:bg-gray-100 dark:placeholder-gray-400 dark:text-black dark:focus:ring-brightViolet dark:focus:border-brightViolet"
+                        className="flex mx-auto p-3 pr-36 mt-3 text-sm md:pr-96 md:text-base text-gray-900 bg-gray-50 rounded-3xl border border-gray-400 focus:ring-brightViolet focus:border-brightViolet dark:bg-gray-100 dark:placeholder-gray-400 dark:text-black dark:focus:ring-brightViolet dark:focus:border-brightViolet"
                         placeholder="Search"
                         required
                     />
                     <button
                         type="submit"
-                        className="  text-brightViolet font-semibold absolute right-9 bottom-2.5 md:right-24 lg:right-656 xl:right-896 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-2xl text-sm px-4 py-2"
+                        className="  text-brightViolet font-semibold absolute right-9 bottom-1 md:right-24 lg:right-656 xl:right-896 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-2xl text-sm px-4 py-2"
                     >
                         <FontAwesomeIcon icon={faMagnifyingGlass} className='w-5'></FontAwesomeIcon>
                     </button>
